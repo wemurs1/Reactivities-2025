837babf3ce8af3c16aaf00a3974870a8dc355235
Stuart Murray <stuart.murray.dev@outlook.com>
2025-02-17 08:50:16 +0000

    styling client

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,13 +1,16 @@
 import { Grid2 } from '@mui/material';
 import ActivityList from './ActivityList';
+import ActivityFilters from './ActivityFilters';
 
 export default function ActivityDashboard() {
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>Activity filter go here</Grid2>
+      <Grid2 size={4}>
+        <ActivityFilters />
+      </Grid2>
     </Grid2>
   );
 }
