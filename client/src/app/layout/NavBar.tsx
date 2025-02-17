e48853f210334e54332c4110f5416e568ca0562d
Stuart Murray <stuart.murray.dev@outlook.com>
2025-02-17 11:15:25 +0000

    add loading indicator

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,15 +1,28 @@
-import { Box, AppBar, Toolbar, Typography, Container, MenuItem } from '@mui/material';
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  MenuItem,
+  LinearProgress,
+} from '@mui/material';
 import { Group } from '@mui/icons-material';
 import { NavLink } from 'react-router';
 import MenuItemLink from '../shared/components/MenuItemLink';
+import { useStore } from '../../lib/hooks/useStore';
+import { Observer } from 'mobx-react-lite';
 
 export default function NavBar() {
+  const { uiStore } = useStore();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
         position='static'
         sx={{
           backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)',
+          position: 'relative',
         }}
       >
         <Container maxWidth='xl'>
@@ -30,6 +43,16 @@ export default function NavBar() {
             <MenuItem>User menu</MenuItem>
           </Toolbar>
         </Container>
+        <Observer>
+          {() =>
+            uiStore.isLoading ? (
+              <LinearProgress
+                color='secondary'
+                sx={{ position: 'absolute', bottom: 0, left: 0, right: 0, height: 4 }}
+              />
+            ) : null
+          }
+        </Observer>
       </AppBar>
     </Box>
   );
