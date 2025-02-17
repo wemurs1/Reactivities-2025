12148d63feb11093fa4ba8a8e083fd63e5a49096
Stuart Murray <stuart.murray.dev@outlook.com>
2025-02-17 11:04:35 +0000

    add computed mobx

--- client/src/lib/stores/counterStore.ts
+++ client/src/lib/stores/counterStore.ts
@@ -3,6 +3,7 @@ import { makeAutoObservable } from 'mobx';
 export default class CounterStore {
   title = 'Counter Store';
   count = 42;
+  events: string[] = [`Initial count is ${this.count}`];
 
   constructor() {
     makeAutoObservable(this);
@@ -10,9 +11,15 @@ export default class CounterStore {
 
   increment = (amount = 1) => {
     this.count += amount;
+    this.events.push(`Incremented ${amount} - count is now ${this.count}`);
   };
 
   decrement = (amount = 1) => {
     this.count -= amount;
+    this.events.push(`Deccremented ${amount} - count is now ${this.count}`);
   };
+
+  get eventCount() {
+    return this.events.length;
+  }
 }
